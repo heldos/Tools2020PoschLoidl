b069745efb37978994c0134d10b3ec15762bf365
poschf <1910237007@students.fh-hagenberg.at>
2020-05-23 13:57:28 +0200

    Weiter testen mit mergen

--- Git/src/Git.java
+++ Git/src/Git.java
@@ -1,7 +1,16 @@
 
 public class Git {
+<<<<<<< HEAD
 
 	
 	
 	public void thisIsANewMethod() { }
+=======
+		
+	
+	public int testMethod() {
+		int x = 5;
+		return x;
+	}
+>>>>>>> 5482a860951782a90db44c54839b5c1e8c8b9159
 }
